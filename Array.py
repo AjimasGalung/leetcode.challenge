0dc737c48fb4badc28d9f193b2fc91a71fdde51d
AjimasGalung <119465049+AjimasGalung@users.noreply.github.com>
2022-12-21 20:58:06 +0700

    Update Array.py
    Here, I put all the leetcode challenge for each category. In this file, I did some challenge related with array and algorithm task.

--- Array.py
+++ Array.py
@@ -7,6 +7,8 @@ def search(nums, target):
     start = 0
     end = len(nums) - 1
     while start <= end:
+        # Here, I use binary search algorithm, looking for the numbers at a middle
+        # and make the conditions
         mid = (start+end)//2
         if nums[mid] > target:
             end = mid - 1
@@ -15,3 +17,26 @@ def search(nums, target):
         else:
             return mid
         return -1
+    
+
+# Challenge : Search Insert Position
+# Explanation : Given a sorted array of distinct integers and a target value, return the index 
+# if the target is found. If not, return the index where it would be if it were inserted in order.
+# Algorithm :
+# - First, we start making the initialization for start index and end index
+def searchposition(nums, target):
+    start = 0
+    end = len(nums) - 1
+    # Second, loop and stop looping after the condition reach
+    while start <= end:
+        # Calculate the middle index
+        mid = (start+end)//2
+        # Conditional
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    # If target is not found, return the start index 
+    return start
